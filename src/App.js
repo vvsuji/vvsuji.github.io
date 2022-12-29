33fa1065b5f92aa0046c58d60557c05f8f696c32
Suji Falcon <suji.falcon@gmail.com>
2022-12-29 13:50:11 -0500

    i'm so sillypilled

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
 				<Navbar />
 				<ScrollToTop />
 				<Routes>
-					<Route path='/vvsuj2-portfolio' element={<Home />} />
+					<Route path='/' element={<Home />} />
 					<Route path='/project' element={<Projects />} />
 					<Route path='/about' element={<About />} />
 					<Route path='/resume' element={<Resume />} />
