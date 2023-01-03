9f8d4811d224cc9c393139b410365a81e13fc34f
Suji Falcon <suji.falcon@gmail.com>
2023-01-03 15:41:15 -0500

    updated again bc no hash

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 				<Navbar />
 				<ScrollToTop />
 				<Routes>
-					<Route path='/' element={<Home />} />
+					<Route exact path='/' element={<Home />} />
 					<Route path='/project' element={<Projects />} />
 					<Route path='/about' element={<About />} />
 					<Route path='/resume' element={<Resume />} />
